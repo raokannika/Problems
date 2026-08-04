95253753f0ee910c1acaceb892198f8e9f2a0528
K Kannika <kkannikarao07@gmail.com>
2026-08-04 20:40:26 +0530

    Time: 3 ms (81.76%), Space: 46.7 MB (67.71%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -1,19 +1,28 @@
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
-        int min = nums[0];
-        int max = nums[0];
-        Set<Integer> elements = new HashSet<>();
-        for( int num : nums){
-            elements.add(num);
-            min = Math.min(num, min);
-            max = Math.max(num, max);
+        HashMap<Integer, Integer> range = new HashMap<>();
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            range.put(nums[i], 1);
         }
-        ArrayList<Integer> ans = new ArrayList<>();
-        for(int i = min; i <= max; i++){
-            if(!(elements.contains(i))){
-                ans.add(i);
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > max){
+                max = nums[i];
+            }
+            if(nums[i] < min){
+                min = nums[i];
             }
         }
-        return ans;
+        ArrayList<Integer> miss = new ArrayList<>();
+        for(int i = min; i <= max; i++){
+            if(range.containsKey(i)){
+                continue;
+            }
+            else{
+                miss.add(i);
+            }
+        }
+        return miss;
     }
 }
