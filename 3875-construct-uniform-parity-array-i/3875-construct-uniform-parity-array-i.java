f11f60cffe89b7586f65030059cdc1ac540786fb
K Kannika <kkannikarao07@gmail.com>
2026-09-02 15:36:02 +0530

    Time: 1 ms (31.89%), Space: 45.1 MB (73.43%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,25 +1,3 @@
-// class Solution {
-//     public boolean uniformArray(int[] nums1) {
-
-//         if (nums1.length < 2) {
-//             return true;
-//         }
-
-//         int check = nums1[1] - nums1[0];
-//         boolean refEven = check % 2 == 0;
-
-//         for (int i = 2; i < nums1.length; i++) {
-//             int diff = nums1[i] - nums1[i - 1];
-//             boolean currEven = diff % 2 == 0;
-
-//             if (currEven != refEven) {
-//                 return false;
-//             }
-//         }
-
-//         return true;
-//     }
-// }
 class Solution {
     public boolean uniformArray(int[] nums1) {
         return true;
