9255a7c0787ab70ab75314fd615b58241aa3c8f5
K Kannika <kkannikarao07@gmail.com>
2026-09-09 18:35:43 +0530

    Time: 0 ms (100%), Space: 43 MB (8.36%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,8 +1,5 @@
 class Solution {
     public int countCommas(int n) {
-        if(n >= 1000){
-            return n - 999;
-        }
-        return 0;
+        return (n >= 1000) ? (n - 999) : 0;
     }
 }
